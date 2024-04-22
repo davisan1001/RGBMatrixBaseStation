f581503487d7cfe21e842b8ce30ce4d2c5049fed
davisan1001 <davisan1001@gmail.com>
2024-04-22 16:31:47 -0300

    Updated to display text for testing

--- my-project/weather-station.cc
+++ my-project/weather-station.cc
@@ -28,54 +28,30 @@ static int usage(const char *progname) {
   return 1;
 }
 
-class DemoRunner {
+class MatrixModule {
  protected:
-  DemoRunner(Canvas *canvas) : canvas_(canvas) {}
-  inline Canvas *canvas() { return canvas_; }
+  MatrixModule() {}
 
  public:
-  virtual ~DemoRunner() {}
-  virtual void Run() = 0;
+  virtual ~MatrixModule() {}
 
  private:
-  Canvas *const canvas_;
+  // TODO: No private members yet
 };
 
 // Simple generator that pulses through RGB and White.
-class ColorPulseGenerator : public DemoRunner {
+class WeatherStationModule : public MatrixModule {
  public:
-  ColorPulseGenerator(RGBMatrix *m) : DemoRunner(m), matrix_(m) {
-    off_screen_canvas_ = m->CreateFrameCanvas();
-  }
-  void Run() override {
-    uint32_t continuum = 0;
-    while (!interrupt_received) {
-      usleep(5 * 1000);
-      continuum += 1;
-      continuum %= 3 * 255;
-      int r = 0, g = 0, b = 0;
-      if (continuum <= 255) {
-        int c = continuum;
-        b = 255 - c;
-        r = c;
-      } else if (continuum > 255 && continuum <= 511) {
-        int c = continuum - 256;
-        r = 255 - c;
-        g = c;
-      } else {
-        int c = continuum - 512;
-        g = 255 - c;
-        b = c;
-      }
-      off_screen_canvas_->Fill(r, g, b);
-      off_screen_canvas_ = matrix_->SwapOnVSync(off_screen_canvas_);
-    }
+  WeatherStationModule() : MatrixModule() {
+    // Do nothing for now
   }
 
  private:
-  RGBMatrix *const matrix_;
-  FrameCanvas *off_screen_canvas_;
 };
+
+void updateFrame() {
+  // Nothing for now...
+}
 
 int main(int argc, char *argv[]) {
   const char *demo_parameter = NULL;
@@ -94,29 +70,35 @@ int main(int argc, char *argv[]) {
     return usage(argv[0]);
   }
 
-  // Now extract any command line flags that are relevant to this program.
-  // TODO: No command line flags are relevant to this program yet...
-  int opt;
-  while ((opt = getopt(argc, argv, "")) != -1) {
-    switch (opt) {
-      default: /* '?' */
-        // return usage(argv[0]);
-        break;
-    }
+  // Setup font
+  const char *bdf_font_file = "../fonts/tom-thumb-fixed_4x6.bdf";
+  int x_orig = 0;  // TODO: No need?
+  int y_orig = 0;  // TODO: No need?
+  int letter_spacing = 0;
+
+  if (bdf_font_file == NULL) {
+    fprintf(stderr, "Unrecognized font file\n");
+    return 1;
   }
 
+  /*
+   * Load font. This needs to be a filename with a bdf bitmap font.
+   */
+  rgb_matrix::Font font;
+  if (!font.LoadFont(bdf_font_file)) {
+    fprintf(stderr, "Couldn't load font '%s'\n", bdf_font_file);
+    return 1;
+  }
+
+  // Initialize RGBMatrix
   RGBMatrix *matrix = RGBMatrix::CreateFromOptions(matrix_options, runtime_opt);
   if (matrix == NULL) return 1;
 
-  // TODO: Purpose?... Debugging?
-  printf("Size: %dx%d. Hardware gpio mapping: %s\n", matrix->width(),
-         matrix->height(), matrix_options.hardware_mapping);
+  FrameCanvas *off_screen_canvas = matrix->CreateFrameCanvas();
 
-  Canvas *canvas = matrix;
-
-  // The DemoRunner objects are filling
+  // The MatrixModule objects are filling
   // the matrix continuously.
-  DemoRunner *demo_runner = new ColorPulseGenerator(matrix);
+  MatrixModule *weatherModule = new WeatherStationModule();
 
   // Set up an interrupt handler to be able to stop animations while they go
   // on. Each demo tests for while (!interrupt_received) {},
@@ -126,11 +108,26 @@ int main(int argc, char *argv[]) {
 
   printf("Press <CTRL-C> to exit and reset LEDs\n");
 
-  // Now, run our particular demo; it will exit when it sees interrupt_received.
-  demo_runner->Run();
+  int text_start_x = 0;
+  int text_start_y = 0;
+  Color text_color(255, 255, 0);
+  char line[1024] = "Hello World!\n Time: 12:45";
 
-  delete demo_runner;
-  delete canvas;
+  DrawText(off_screen_canvas, font, text_start_x,
+           text_start_y + font.baseline(), text_color, NULL, line,
+           letter_spacing);
+
+  // off_screen_canvas->
+  off_screen_canvas = matrix->SwapOnVSync(off_screen_canvas);
+
+  // ~~~ MAIN LOOP ~~~
+  while (!interrupt_received) {
+    usleep(5 * 1000);
+  }
+  // ~~~ END ~~~
+
+  delete weatherModule;
+  // delete canvas;
 
   printf("Received CTRL-C. Exiting.\n");
   return 0;
