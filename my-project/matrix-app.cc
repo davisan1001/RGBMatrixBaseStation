081e249ec4fb8e04fdfffa21bda45201d55a652a
davisan1001 <davisan1001@gmail.com>
2024-06-27 19:10:13 -0300

    Fixed a stupid mistake on variable initialization.

--- my-project/matrix-app.cc
+++ my-project/matrix-app.cc
@@ -96,17 +96,17 @@ class WeatherStationModule : public MatrixModule {
   WeatherStationModule(rgb_matrix::RGBMatrix *m) : MatrixModule(m) {
     // Setup default colors
     seperator_color = rgb_matrix::Color(84, 84, 84);
-    rgb_matrix::Color temp_high_color = rgb_matrix::Color(255, 126, 0);
-    rgb_matrix::Color temp_low_color = rgb_matrix::Color(0, 183, 239);
-    rgb_matrix::Color clock_color = rgb_matrix::Color(255, 255, 255);
-    rgb_matrix::Color date_color = rgb_matrix::Color(
+    temp_high_color = rgb_matrix::Color(255, 126, 0);
+    temp_low_color = rgb_matrix::Color(0, 183, 239);
+    clock_color = rgb_matrix::Color(255, 255, 255);
+    date_color = rgb_matrix::Color(
         120, 120, 120);  // Grey (consider changing for visibility)
-    rgb_matrix::Color current_weekday_color = rgb_matrix::Color(111, 49, 152);
-    rgb_matrix::Color future_weekday_color = rgb_matrix::Color(
+    current_weekday_color = rgb_matrix::Color(111, 49, 152);
+    future_weekday_color = rgb_matrix::Color(
         120, 120, 120);  // Grey (consider changing for visibility)
-    rgb_matrix::Color temp_predicted_low_color = rgb_matrix::Color(
+    temp_predicted_low_color = rgb_matrix::Color(
         161, 161, 161);  // Grey (consider changing for visibility)
-    rgb_matrix::Color temp_predicted_high_color = rgb_matrix::Color(
+    temp_predicted_high_color = rgb_matrix::Color(
         120, 120, 120);  // Grey (consider changing for visibility);
   }
 
